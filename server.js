98d5f0e7e1aecd86edbd71229d94fc2198acb06a
abdullah-aljoulani <abdullahaljoulani@gmail.com>
2023-07-24 11:36:37 +0000

    fix deploy

--- server.js
+++ server.js
@@ -2,10 +2,13 @@
 
 const express = require('express')
 
+require("dotenv").config();
+
 const app = express()
 
 app.get('/',(req,res)=>{
     res.status(200).json('welcome to home page')
 })
+const port = process.env.PORT || 3001
 
-app.listen(3000,()=> console.log('running on port',3000))
+app.listen(port,()=> console.log('running on port',port))
